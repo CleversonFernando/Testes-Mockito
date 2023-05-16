cff39e20f2f28bb1375b603da08bce6d908ffe69
Cleverson Fernando Rodrigues <cfrodrigues50@gmail.com>
2023-05-16 11:50:56 -0300

    User classe usuário

---
+++ src/main/java/com/cleversonfernando/casodetestesmockito/domain/User.java
@@ -0,0 +1,39 @@
+package com.cleversonfernando.casodetestesmockito.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
