4c83b6c589c369563b75eb667819d31fe06581b2
Cleverson Fernando Rodrigues <cfrodrigues50@gmail.com>
2023-05-16 11:32:10 -0300

    Projeto iniciado

---
+++ src/test/java/com/cleversonfernando/casodetestesmockito/CasodetestesmockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleversonfernando.casodetestesmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasodetestesmockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
