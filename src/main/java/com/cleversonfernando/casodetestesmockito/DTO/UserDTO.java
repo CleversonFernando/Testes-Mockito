3d2ef588f6c449c696869701950752e43daf8e83
Cleverson Fernando Rodrigues <cfrodrigues50@gmail.com>
2023-05-16 16:28:21 -0300

    Dependência para modelMapper

---
+++ src/main/java/com/cleversonfernando/casodetestesmockito/DTO/UserDTO.java
@@ -0,0 +1,18 @@
+package com.cleversonfernando.casodetestesmockito.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
