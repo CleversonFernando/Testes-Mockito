bed8992d1cc8dc4c4452d22933be2cca84e1bf6b
Cleverson Fernando Rodrigues <cfrodrigues50@gmail.com>
2023-06-29 19:41:06 -0300

    Corrigindo bug

--- src/test/java/com/cleversonfernando/casodetestesmockito/controller/UserControllerTest.java
+++ src/test/java/com/cleversonfernando/casodetestesmockito/controller/UserControllerTest.java
@@ -30,8 +30,8 @@ class UserControllerTest {
     public static final String PASSWORD = "123";
     public static final int INDEX = 0;
 
-    private User user;
-    private UserDTO userDTO;
+    private User user = new User();
+    private UserDTO userDTO = new UserDTO();
     @InjectMocks
     private UserController userController;
     @Mock
